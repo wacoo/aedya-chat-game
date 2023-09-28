5650191388c699360de4c839592bad57e33047d0
wac <wabaham9@gmail.com>
2023-09-29 00:50:57 +0300

    Add score in progress

--- server/api/v1/view.py
+++ server/api/v1/view.py
@@ -94,19 +94,24 @@ def new_game():
 #@auth.requires_token
 def getGame():
    ''' get game '''
-   email = request.args.get("email")
-   #games = session.query(Games).all()
-   game1 = session.query(Games).filter(or_(Games.player1 == email, Games.player2 == email), Games.done == False).first()
-   opponent = ''
-   
-   if game1:
-      if game1.player2 == email:
-         opponent = game1.player1
-      elif game1.player1 == email:
-         opponent = game1.player2
-      return {'message': 'Game found', 'game_id': game1.id, 'opponent': opponent} #jsonify({'id':game1.id, 'player1': game1.player1, 'player2': game1.player2})
-   else:
-      return jsonify({'error': 'No game in progress'})
+   try:
+      email = request.args.get("email")
+      #games = session.query(Games).all()
+      game1 = session.query(Games).filter(or_(Games.player1 == email, Games.player2 == email), Games.done == False).first()
+      opponent = ''
+      
+      if game1:
+         if game1.player2 == email:
+            opponent = game1.player1
+         elif game1.player1 == email:
+            opponent = game1.player2
+         return {'message': 'Game found', 'game_id': game1.id, 'opponent': opponent} #jsonify({'id':game1.id, 'player1': game1.player1, 'player2': game1.player2})
+      else:
+         return jsonify({'error': 'No game in progress'})
+      session.commit()
+   except Exception as e:
+      session.rollback()
+      return jsonify({'error': 'Can\'t get game data!'})
 
 @view.route('/updatecount', methods=['PUT'])
 #@auth.requires_token
@@ -121,3 +126,88 @@ def update_count():
    except Exception as e:
       session.rollback()
       return jsonify({'error': 'Update not successful: ' + e})
+
+@view.route('/addeval', methods=['PUT'])
+#@auth.requires_token
+def add_evaluation():
+   ''' add evaluation '''
+   game_id = request.json.get('game_id')
+   user_email = request.json.get('email')
+   review_value = request.json.get('value')
+
+   try:
+      game = session.query(Games).filter(Games.id == game_id).first()
+      if user_email == game.player1:
+         game.player1_review = review_value
+      elif user_email == game.player2:
+         game.player2_review = review_value
+      session.commit()
+      return jsonify({'message': 'Update successful!'})
+   except Exception as e:
+      session.rollback()
+      return jsonify({'error': 'Update not successful: ' + str(e)})
+
+
+@view.route('/addscore', methods=['PUT'])
+#@auth.requires_token
+def add_score():
+   ''' add score '''
+   try:
+      game_id = request.json.get('game_id')
+      game = session.query(Games).filter(Games.id == game_id).first()
+      player1_review = game.player1_review
+      player2_review = game.player2_review
+
+      if player1_review < 0 or player2_review < 0:
+         return jsonify({'error': 'Review not complete!', 'success': False})
+
+      player1_review = max(player1_review, 0)
+      player2_review = max(player2_review, 0)
+
+      if player1_review == player2_review:
+         player1_score = 1
+         player2_score = 1
+      else:
+         if player1_review == 1:
+               player1_score = 0
+               player2_score = 3
+         else:
+               player1_score = 3
+               player2_score = 0
+
+      print(player1.total_score + player1_score)
+      player1 = session.query(User).filter(User.email == game.player1).first()
+      player1.total_score += player1_score
+
+      player2 = session.query(User).filter(User.email == game.player2).first()
+      player2.total_score += player2_score
+
+      session.commit()
+      return ({'message': 'Score added successfully!', 'scores': {player1.email: player1.total_score, player2.email: player2.total_score}, 'success': True  })
+   except Exception as e:
+      session.rollback()
+      return jsonify({'error': 'Score not added! ' + str(e)})
+   
+@view.route('/getuser', methods=['GET'])
+#@auth.requires_token
+def get_user():
+   ''' get user '''
+   try:
+      email = request.args.get('email')
+      user = session.query(User).filter(User.email == email).first()
+
+      if user:
+         user_data = {
+            'email': user.email,
+            'fname': user.fname,
+            'lname': user.lname,
+            'country': user.country,
+            'total_score': user.total_score,
+            # ...include other attributes as needed
+         }
+      session.commit()
+      return jsonify(user_data)
+   except Exception as e:
+      session.rollback()
+      return jsonify({'error': 'Can\'t load data for ' + email})
+
--- server/models/games.py
+++ server/models/games.py
@@ -4,6 +4,7 @@ from .base import Base, engine
 import datetime
 
 class Games(Base):
+    ''' games class '''
     __tablename__ = 'games'
     id = Column(Integer, primary_key=True)
     name = Column(String(200))
@@ -13,15 +14,20 @@ class Games(Base):
     winner = Column(String(50))
     done = Column(Boolean)
     chat_count = Column(Integer)
+    player1_review = Column(Integer)
+    player2_review = Column(Integer)
+    chat_count = Column(Integer)
     created_at = Column(DateTime, default=datetime.datetime.utcnow)
     updated_at = Column(DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
 
-    def __init__(self, name, description, player1, player2, winner='', done=False):
+    def __init__(self, name, description, player1, player2, player1_review=-1, player2_review=-1, winner='', done=False):
         self.name = name
         self.description = description
         self.player1 = player1
         self.player2 = player2
         self.winner = winner
         self.done = done
+        self.player1_review = player1_review
+        self.player2_review = player2_review
         self.chat_count = 0
 Base.metadata.create_all(bind=engine)
